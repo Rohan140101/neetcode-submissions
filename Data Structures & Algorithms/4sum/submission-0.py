79e2be5b2772f5a699222907c427702a585bd166
Rohan Kuckian <56680883+Rohan140101@users.noreply.github.com>
2026-06-24 19:39:38 -0400

    Add: 4sum - submission-0

---
+++ Data Structures & Algorithms/4sum/submission-0.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        # Hashmap
+        nums.sort()
+        hashmap = defaultdict(int)
+        n = len(nums)
+        for i in range(n):
+            hashmap[nums[i]] += 1
+
+
+        results = set()
+        for i in range(n-2):
+            for j in range(i+1, n-1):
+                for k in range(j+1, n):
+                    newMap = hashmap.copy()
+                    newMap[nums[i]] -= 1
+                    newMap[nums[j]] -= 1
+                    newMap[nums[k]] -= 1
+                    valToGet = target - nums[i] - nums[j] - nums[k]
+                    if newMap[valToGet] > 0:
+                        ans = [nums[i], nums[j], nums[k], valToGet]
+                        ans.sort()
+                        results.add(tuple(ans))
+
+        results = [list(x) for x in results]
+
+        return results
+        
