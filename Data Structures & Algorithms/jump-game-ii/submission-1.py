5f782402f3728774ec036db5a72ac2d464be705f
Rohan Kuckian <56680883+Rohan140101@users.noreply.github.com>
2026-07-03 22:15:43 -0400

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.py
@@ -0,0 +1,47 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        # minJumps = float('inf')
+        # dp = [None for i in range(n)]
+        # def dfs(i):
+        #     nonlocal dp
+        #     if i == n-1:
+        #         # minJumps = min(minJumps, jumps)
+        #         # return
+        #         dp[i] = 0
+        #         return 0
+
+
+        #     if i >= n:
+        #         return float('inf')
+
+        #     if dp[i] != None:
+        #         return dp[i]
+
+        #     jumps = float('inf')
+        #     for k in range(nums[i], 0, -1):
+        #         jumps = min(jumps, 1+ dfs(i+k))
+
+        #     dp[i] = jumps
+        #     return jumps
+
+        # dfs(0)
+        # return dp[0]
+
+
+        result = 0
+        l, r = 0, 0
+        # f = 0
+        while r < n-1:
+            f = r
+            for i in range(l, r+1):
+                f = max(f, i + nums[i])
+
+            l = r + 1
+            r = f
+            result += 1
+        return result
+
+            
+        
